a762813eb19e437184614766545b00876d9be502
geunyoung490 <geunyoung490@inu.ac.kr>
2023-03-22 18:11:17 +0900

    findById()

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +17,11 @@ class DemoApplicationTests {
 
 	@Test
 	void testJpa() {
-		List<Question> all = this.questionRepository.findAll();
-		assertEquals(2,all.size());
-
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?",q.getSubject());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()){
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?",q.getSubject());
+		}
 	}
 
 }

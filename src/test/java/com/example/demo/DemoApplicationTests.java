6e53b8956faa3bedde726da97056a7b83453c058
geunyoung490 <geunyoung490@inu.ac.kr>
2023-03-22 19:33:03 +0900

    답변에 연결된 질문 조회

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
@@ -18,21 +19,17 @@ import java.util.Queue;
 @SpringBootTest
 class DemoApplicationTests {
 	@Autowired
-	private  QuestionRepository questionRepository;
-	@Autowired
-	private AnswerRepository answerRepository;
-
+	private QuestionRepository questionRepository;
+	@Transactional
 	@Test
 	void testJpa() {
 		Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
-		}
-	}
+		List<Answer> answerList = q.getAnswerList();
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 
+	}
+}

7177906108461931b1071df2b014dd32fd523347
geunyoung490 <geunyoung490@inu.ac.kr>
2023-04-10 22:23:35 +0900

    작성일지 역순으로 조회

--- src/main/java/com/example/demo/question/QuestionService.java
+++ src/main/java/com/example/demo/question/QuestionService.java
@@ -1,11 +1,13 @@
 package com.example.demo.question;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import com.example.demo.DataNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import java.time.LocalDateTime;
@@ -27,7 +29,9 @@ public class QuestionService {
         }
     }
     public Page<Question> getList(int page){
-        Pageable pageable = PageRequest.of(page, 10);
+        List<Sort.Order> sorts= new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page,10 ,Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
     public void create(String subject, String content){

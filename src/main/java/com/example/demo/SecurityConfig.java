fa551a148ce19064ba2ebf9a411af08a1717efd2
geunyoung490 <geunyoung490@inu.ac.kr>
2023-04-11 00:02:09 +0900

    H2 콘솔 예외처리

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -12,7 +13,14 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/**")).permitAll();
+        http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+                .and()
+                    .csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                .and()
+                    .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+
         return http.build();
     }
 }

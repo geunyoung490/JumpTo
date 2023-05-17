55538eff3e6ee3e97c8ee04046c5a3e37113b31a
geunyoung490 <geunyoung490@inu.ac.kr>
2023-05-17 21:40:35 +0900

    질문에 작성자 저장하기

--- src/main/java/com/example/demo/question/QuestionController.java
+++ src/main/java/com/example/demo/question/QuestionController.java
@@ -1,8 +1,11 @@
 package com.example.demo.question;
 
+import java.security.Principal;
 import java.util.List;
 
 import com.example.demo.answer.AnswerForm;
+import com.example.demo.user.SiteUser;
+import com.example.demo.user.UserService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
+    private final UserService userService;
     @GetMapping("/list")
     public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page){
         Page<Question> paging = this.questionService.getList(page);
@@ -38,11 +42,12 @@ public class QuestionController {
         return "question_form";//th:object에 의해 QuestionForm 객체가 필요
     }
     @PostMapping("/create")
-    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult){
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal){
         if(bindingResult.hasErrors()){
             return "question_form";
         }
-        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        SiteUser siteUser= this.userService.getUser(principal.getName())
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
         return "redirect:/question/list";// 질문 저장 후 질문목록으로 이동.
     }
 }

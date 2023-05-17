fed88255da2e1642fcbd46050b400f566696af87
geunyoung490 <geunyoung490@inu.ac.kr>
2023-05-17 21:33:12 +0900

    답변에 작성자 저장하기

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,9 +1,12 @@
 package com.example.demo.user;
 
+import com.example.demo.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +23,13 @@ public class UserService {
         this.userRepository.save(user);
         return user;
     }
-
+    public SiteUser getUser(String username){
+        Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+        if(siteUser.isPresent()){
+            return siteUser.get();
+        }
+        else{
+            throw  new DataNotFoundException("siteuser not found");
+        }
+    }
 }

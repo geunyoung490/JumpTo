67d7d125865add1848dc23eed000cca29c147335
geunyoung490 <geunyoung490@inu.ac.kr>
2023-05-22 00:32:00 +0900

    답변 수정

--- src/main/java/com/example/demo/answer/AnswerService.java
+++ src/main/java/com/example/demo/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.example.demo.answer;
 
+import com.example.demo.DataNotFoundException;
 import com.example.demo.question.Question;
 import com.example.demo.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,6 +20,22 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+    //답변조회
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        }
+        else{
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+    //답변수정
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
